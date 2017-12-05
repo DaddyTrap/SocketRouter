8f16bc91f494999069d13b0306cf29b720a2deb8
Sevennn <418057982@qq.com>
2017-12-05 20:37:34 +0800

    :bug: fixed bug in dv

--- routeAlgos/dv.py
+++ routeAlgos/dv.py
@@ -1,8 +1,8 @@
 def dv_algo(other_node, other_cost,source,source_cost,forward):
+    changeFlag = False
     for k,v in other_cost.items():
-        if k not in source_cost:
+        if k not in source_cost or source_cost[k] > source_cost[other_node] + other_cost[k]:
             source_cost[k] = source_cost[other_node]+other_cost[k]
             forward[k] = other_node
-        elif source_cost[k] > source_cost[other_node] + other_cost[k]:
-            source_cost[k] = source_cost[other_node] + other_cost[k]
-            forward[k] = other_node
+            changeFlag = True
+    return changeFlag
